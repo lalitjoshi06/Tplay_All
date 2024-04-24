ead45b0ab50c3acf42f1e07089e626e74bc30a93
lalitjoshi06 <ljoshi49@gmail.com>
2024-04-24 22:29:53 +0530

    Logo res to low

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -118,7 +118,7 @@ const generateM3u = async (ud) => {
 
             for (let i = 0; i < chansList.length; i++) {
                 m3uStr += '#EXTINF:-1 tvg-id="' + chansList[i].id.toString() + '" ';
-                m3uStr += 'group-title="' + chansList[i].group_title + '", tvg-logo="' + chansList[i].tvg_logo + '", ' + chansList[i].name + '\n';
+                m3uStr += 'group-title=\"' + (chansList[i].group_title) + '\", tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/' + (chansList[i].tvg_logo) + '\", ' + chansList[i].name + '\n';
                 m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
                 m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
                 m3uStr += chansList[i].stream_url + '?' + chansList[i].hma + '\n\n';
