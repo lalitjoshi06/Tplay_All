edb14b306a9fba5bf3573eccecfcb16e68122a01
Ajit Singh Yadav <37411261+AjitGE@users.noreply.github.com>
2024-05-02 00:16:24 +0530

    updated

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -105,18 +105,18 @@ const generateM3u = async (ud) => {
                 m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
                 m3uStr += '#EXTVLCOPT:http-user-agent=' + chansList[i].stream_headers + '\n';
                // m3uStr += 'catchup="default" '
-                if(chansList[i].stream_url.includes("bpweb")){
-                    let catup_stream_url = chansList[i].stream_url.split(".")[0].replace("bpweb","bpprod")+"catchup"
-                    const splitString=chansList[i].stream_url.split(".")
-                        for(let i=1; i<splitString.length;i++){
-                           catup_stream_url  = catup_stream_url+ "." +splitString[i]
+                // if(chansList[i].stream_url.includes("bpweb")){
+                //     let catup_stream_url = chansList[i].stream_url.split(".")[0].replace("bpweb","bpprod")+"catchup"
+                //     const splitString=chansList[i].stream_url.split(".")
+                //         for(let i=1; i<splitString.length;i++){
+                //            catup_stream_url  = catup_stream_url+ "." +splitString[i]
 
-                        }
-                        m3uStr += 'catchup-source="'+catup_stream_url + '?'+catchupTime+ '"\n';
-                    }
-                    else {
-                        m3uStr += 'catchup-source="'+chansList[i].stream_url + '?'+catchupTime+'"\n';
-                    }
+                //         }
+                //         m3uStr += 'catchup-source="'+catup_stream_url + '?'+catchupTime+ '"\n';
+                //     }
+                //     else {
+                //         m3uStr += 'catchup-source="'+chansList[i].stream_url + '?'+catchupTime+'"\n';
+                //     }
                 //m3uStr+=  'catchup-days="7" catchup-correction="+5.30"\n'
                 m3uStr += chansList[i].stream_url+'?'+chansList[i].hma+'\n\n'
             }
